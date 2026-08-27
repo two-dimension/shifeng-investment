59996b6b38a3962728ba76796b4af39f3a639107
Raymond Rui Xing Wang <ray_wang@RaymonddeMacBook-Pro.local>
2026-08-27 13:21:13 +0800

    chore: exclude unrelated ICE CDS changes

--- server/lib/iceCdsSnapshotWriteQueue.js
+++
@@ -1,9 +0,0 @@
-let writeTail = Promise.resolve();
-
-/** Serializes every process-local read/modify/write of the shared dashboard snapshot. */
-export function enqueueIceCdsSnapshotWrite(operation) {
-  if (typeof operation !== 'function') throw new TypeError('snapshot write operation must be a function');
-  const queued = writeTail.then(operation);
-  writeTail = queued.catch(() => undefined);
-  return queued;
-}
